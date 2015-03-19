ac90e6ac004da290145c650153235d8d6117617c
wind1689 <wind1689@gmail.com>
2015-03-19 15:40:56 -0700

    Update BotProtocol.h

--- BotProtocol.h
+++ BotProtocol.h
@@ -1,10 +1,10 @@
 const char * victim = "server.team4.uclaclass.isi.deterlab.net";
-/*		
+		
 // Add the attack times and durations into one char[];		
-char attackTime[512] = "15:48:00.000000"   // Attark1 Start time;		
-			"0002"             // Attack1 duration(seconds);		
+char attackTime[512] = "15:42:00.000000"   // Attark1 Start time;		
+			"0100"             // Attack1 duration(seconds);		
 			"8000"             // Attack1 attack rate(#/second);   (7000 Max)		
-			"0"                // Attack1 IP Spoofing method; (0-No;1-Linear;2-Random)		
+			"2"                // Attack1 IP Spoofing method; (0-No;1-Linear;2-Random)		
 			"15:48:30.000000"  // Attack2 Start time;		
 			"0003"             // Attack2 duration(seconds);		
 			"5000"             // Attack2 attack rate(#/second);		
@@ -13,8 +13,8 @@ char attackTime[512] = "15:48:00.000000"   // Attark1 Start time;
 			"0004"             // Attack3 duration(seconds);		
 			"1000"             // Attack3 attack rate(#/second);		
 			"2";               // Attack3 IP Spoofing method; (0-No;1-Linear;2-Random)		
-*/		
 		
+/*		
 // Fix attack duration & attack rate, change ip spoof method;		
 char attackTime[800] = "15:40:00.000000"   // Attark1 Start time;		
 			"0010"             // Attack1 duration(seconds);		
@@ -119,6 +119,7 @@ char attackTime[800] = "15:40:00.000000"   // Attark1 Start time;
 			"0001"             // Attack3 duration(seconds);		
 			"5000"             // Attack3 attack rate(#/second);		
 			"2";               // Attack3 IP Spoofing method; (0-No;1-Linear;2-Random)
+*/
 // Two set of IP addresses, first for deterlab, the second 
 // for local test
 
